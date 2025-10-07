f346fdf22e81eab678ca2a1efdb5823d3b5031e1
Pungetello <Pungetello@gmail.com>
2025-10-07 11:52:06 -0600

    Makes platform folder if not exists

--- Downloader.R
+++ Downloader.R
@@ -3,11 +3,13 @@ library(GEOquery)
 
 
 #-----------downloading_data-----------
-download_data = function(accession_id_list, desired_path){
+download_data = function(accession_id_list, platform_folder){
   original_wd = getwd()
   # Create and set the working directory to your desired path
-  dir.create(desired_path)
-  setwd(desired_path)
+  if (!dir.exists(platform_folder)){
+    dir.create(platform_folder)
+  }
+  setwd(platform_folder)
   # Specify the GSE accession number and then download to a new subdirectory named after accession number
   for (accession_id in accession_id_list) {
     gse_accession = accession_id
