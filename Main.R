651d5cbbdc01c56268006889cc9e846dc597bb8d
Pungetello <Pungetello@gmail.com>
2025-10-10 09:42:48 -0600

    Updated main in lab meeting

--- Main.R
+++ Main.R
@@ -1,15 +1,11 @@
-#packages
-source("PlatformsList.R")
-
 #installs packages that are necessary for later steps. These two scripts only need to be executed once
 source("InstallCorePackages.R")
-source("Downloader.R")
 source("InstallArrayPackages.R")
 
 #execute every time:
-#source(LibraryInstaller.R)
+source("Downloader.R")
 source("ColumnCleaner.R")
-#source(Statistical_tester)
+#source(StatisticalTester)
 
 
 
