c85a5b22d2b8cef6a0f6c65daa8954473e7f39c5
Pungetello <Pungetello@gmail.com>
2025-10-10 15:12:03 -0600

    Lab meeting changes

--- Main.R
+++ Main.R
@@ -1,6 +1,6 @@
 #installs packages that are necessary for later steps. These only need to be executed once
 source("InstallCorePackages.R")
-source("Downloader.R")
+source("DownloadBrainarrayExamples.R")
 source("InstallArrayPackages.R")
 
 #execute every time:
