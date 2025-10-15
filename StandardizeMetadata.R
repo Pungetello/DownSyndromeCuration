d57630fccfb8767de01f22ff8f5b8592420eddc8
Pungetello <Pungetello@gmail.com>
2025-10-15 13:48:47 -0600

    added select_attributes

--- StandardizeMetadata.R
+++ StandardizeMetadata.R
@@ -11,6 +11,7 @@ get_metadata = function(series_ID) {
   metadata = getGEO(series_ID)[[1]] 
   
   metadata = as_tibble(pData(metadata))
+  print(names(metadata))
   return (metadata)
 }
 
@@ -28,7 +29,56 @@ drop_cols = function(metadata, file_location, series_ID) {
   metadata_filtered = metadata %>% 
   select(-cols_to_drop)
   
-  write_tsv(metadata_filtered, paste0(file_location, series_ID, "_metadata.tsv"))
+  return(metadata_filtered)
+  #write_tsv(metadata_filtered, paste0(file_location, series_ID, "_metadata.tsv"))
+}
+
+select_attributes = function(geo_id, metadata, target_attributes_tibble){
+  
+  # loop through all rows in metadata
+  for (i in 1:nrow(metadata)){ 
+    row = metadata[i, ]
+    
+    current_sex = NA
+    current_ploidy = NA
+    current_cell_type = NA
+    ID = NA
+    
+    
+    #look for cell_type
+    column_name = names(metadata)[grepl("cell type", names(metadata)) | # get column name for cell type
+                                     grepl("cell_type", names(metadata))]
+    if (length(column_name) == 0){
+      column_name = names(metadata)[grepl("tissue", names(metadata))]
+    }
+    if (length(column_name) != 0) { # if column name exists
+      current_cell_type = row[[column_name]] # current cell type is the value in that column
+    }
+    
+    # look for ID
+    column_name = names(metadata)[grepl("geo", names(metadata))] # get column name for ID
+    if (length(column_name) != 0) { # if column name exists
+      ID = row[[column_name]] # current ID is the value in that column
+    }
+    
+    #look for sex
+    if (any(str_detect(row, regex("female", ignore_case = TRUE)), na.rm = TRUE)){ # first check each cell in row for 'female' if found set current sex to female
+      current_sex = "female" 
+    } else if (any(str_detect(row, regex("male", ignore_case = TRUE)), na.rm = TRUE)){
+      current_sex = "male"
+    }
+    
+    #look for ploidy
+    if (any(str_detect(row, regex("trisomic | trisomy | down syndrome | ts21", ignore_case = TRUE)), na.rm = TRUE)){ # first check each cell in row for 'trisomic' if found set current ploidy to trisomic
+      current_ploidy = "trisomic"
+    } else if (any(str_detect(row, regex("disomic | disomy | WT | normal | control | euploid", ignore_case = TRUE)), na.rm = TRUE)){
+      current_ploidy = "disomic"
+    }
+    
+    target_attributes_tibble = add_row(target_attributes_tibble, geo_accession = ID , sex = current_sex, ploidy = current_ploidy, cell_type = current_cell_type) # append to tiblle with row contained retrieved values
+  }
+  #file.remove(temp_file_location) # remove temporary file
+  return(target_attributes_tibble)
 }
 
 
@@ -40,10 +90,19 @@ if (!dir.exists(file_location)){
   dir.create(file_location, recursive = TRUE)
 }
 
+# create tibble to store target attributes
+target_attributes_tibble = tibble ( 
+  geo_accession = character(), sex = character(), ploidy = character(), cell_type = character()   #TODO: clean up somewhow idk
+  
+)
+
 # loop through all series IDs
 for (geo_id in names(platforms_list)) {
   # read metadata into a variable, drop unneeded columns, and save it
   metadata = get_metadata(geo_id) 
-  drop_cols(metadata, file_location, geo_id)
+  filtered_metadata = drop_cols(metadata, file_location, geo_id)
+  target_attributes_tibble = select_attributes(geo_id, filtered_metadata, target_attributes_tibble)
 }
+
+print(target_attributes_tibble, n = Inf)
 
