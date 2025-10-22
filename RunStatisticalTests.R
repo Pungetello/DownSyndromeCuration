c3067df123378e20ecb948602c71b62e5c4956f8
Spencer Nadauld <spencernadauld@SpencersAir173.app.byu.edu>
2025-10-22 13:54:22 -0600

    statistical tests pseudocode

--- RunStatisticalTests.R
+++ RunStatisticalTests.R
@@ -3,7 +3,10 @@
 
 
 #-----------functions------------------
-
+quality_control_removal(
+  # read the data into celfiles
+  #do the three statistical tests -- remove array data that fails all three
+)
 
 
 #-----------run statistical tests-----------
