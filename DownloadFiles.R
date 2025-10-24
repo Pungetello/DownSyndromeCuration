67ad82d55634c587978cba7bd8632b15e62b43a1
Pungetello <Pungetello@gmail.com>
2025-10-24 17:05:25 -0600

    Fixed DownloadData function

--- DownloadFiles.R
+++ DownloadFiles.R
@@ -9,11 +9,11 @@ DownloadData = function(accession_id_list, platform_folder){
     dir.create(platform_folder, recursive = TRUE)
   }
   # Specify the GSE accession number and then download to a new subdirectory named after accession number
-  for (accession_id in accession_id_list) {
+  for (geo_id in accession_id_list) {
     gse_accession = accession_id
     getGEOSuppFiles(gse_accession, makeDirectory = TRUE, baseDir = platform_folder)
     
-    geo_id_dir = sprintf("%s/BrainArrayExamples/%s", getwd(), geo_id)  #directory where the tar file is
+    geo_id_dir = sprintf("%s/%s/%s", getwd(), platform_folder, geo_id)  #directory where the tar file is
     
     tar_file = sprintf("%s/%s_RAW.tar", geo_id_dir, geo_id)
     untar(tar_file, exdir = geo_id_dir)
