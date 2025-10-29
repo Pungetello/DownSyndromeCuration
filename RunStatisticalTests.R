7b87b27b0254521e127ea37098f6fcbc23d041f2
Pungetello <Pungetello@gmail.com>
2025-10-29 14:02:41 -0600

    cleaned up

--- RunStatisticalTests.R
+++ RunStatisticalTests.R
@@ -17,26 +17,17 @@ quality_control_removal = function(file_list, platform, geo_id){
   
   unlink("QualityOutput", recursive = TRUE) #TODO: figure out how to not download in first place
   
-  
   outlierIndices = c(test_results$modules$heatmap@outliers@which, test_results$modules$boxplot@outliers@which, test_results$modules$maplot@outliers@which)
   # Get the assay names corresponding to the indices above from the arrayTable data frame.
   outlierNames = test_results$arrayTable$sampleNames[outlierIndices]
   
-  # Now we have to count occurences of each assay name in the outlierNames
-  # vector. Any that are in three times and were thus classified as outliers
-  # by all three methods should be rejected.
-  # This part uses sapply() to go through the unique assay names found in the
-  # outlierNames vector, and use length() and which() to count how many times each
-  # one appears. #TODO: make this comment shorter
-  # count occurences of each 
+  # count occurrences of each assay name in outlierNames: if it appears 3 times, it is an outlier and should be removed
   counts = sapply(unique(outlierNames), function(x) {
     tests_failed = length(which(outlierNames == x))
   })
-  
-  
     
-  print("REJECTED ASSAYS:\t") #debug
-  print(paste(names(which(counts == 3)), collapse="\t")) #debug
+  # print("REJECTED ASSAYS:\t") #debug
+  # print(paste(names(which(counts == 3)), collapse="\t")) #debug
   
   # Keep files that do not fail all three tests
   accepted_indices = which(counts != 3)
@@ -49,17 +40,16 @@ quality_control_removal = function(file_list, platform, geo_id){
   num_samples = length(gsm_ids)
 
   current_platforms = rep(platform, num_samples)
-  #current_platforms = gsub("\t", " ", current_platforms) 
   pass_reject_tibble = add_column(pass_reject_tibble, platform = current_platforms)
   
   current_geo_id = rep(geo_id, num_samples)
   pass_reject_tibble = add_column(pass_reject_tibble, geo_id = current_geo_id)
 
   rejected_gsms = names(which(counts == 3))
-  rejected_gsms <- str_extract(rejected_gsms, "GSM\\d+")
-  pass_status <- !(gsm_ids %in% rejected_gsms)
+  rejected_gsms = str_extract(rejected_gsms, "GSM\\d+")
+  pass_status = !(gsm_ids %in% rejected_gsms)
   pass_reject_tibble <- add_column(pass_reject_tibble, pass_status)
-  print(pass_reject_tibble) # debug
+  # print(pass_reject_tibble) # debug
   
   write_tsv(pass_reject_tibble, "Data/quality_output_file.tsv", append = TRUE, col_names = FALSE)
   
@@ -115,8 +105,7 @@ get_scan_upc_files <- function(cel_files_id, platform_to_package_list, platform)
 #TODO: add platforms to package list for normalization: waiting on website
 
 
-#for (geo_id in names(platforms_list)){
-geo_id = "GSE1789"
+for (geo_id in names(platforms_list)){
   file_data = sprintf("%s/Data/Files/%s", getwd(), geo_id)
   file_list = list.files(path = file_data, pattern="^[^.]*\\.CEL\\.gz$", full.names= TRUE, ignore.case = TRUE)
   filtered_file_list = quality_control_removal(file_list, platforms_list[[geo_id]], geo_id)
@@ -124,5 +113,4 @@ geo_id = "GSE1789"
   #normalized = get_scan_upc_files(file_list, platform_to_package_list, platform)
   #save_normalized_file(geo_id, platform, normalized)
   
-#}
-
+}
