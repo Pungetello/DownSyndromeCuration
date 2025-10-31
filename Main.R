66a53d6ed4bb14aa54c0c0720eaccb6e65a4b031
Pungetello <Pungetello@gmail.com>
2025-10-31 17:45:30 -0600

    managing todos in main

--- Main.R
+++ Main.R
@@ -6,12 +6,10 @@ source("DownloadFiles.R")
 # source("InstallCorePackages.R")
 ##Download Brainarray Examples
 DownloadData(create_unique_vector(platforms_list), "BrainArrayExamples")
-# source("InstallArrayPackages.R")  #TODO: finish this once website is back up           #TODO: ask if we should just download all the data at the beginning
+# source("InstallArrayPackages.R")  #TODO: finish this once website is back up
 
 
 #execute every time:
 DownloadData(names(platforms_list), "Data/Files")
 source("StandardizeMetadata.R")
 # source("RunStatisticalTests.R")
-
-#TODO: add col headers or something.
