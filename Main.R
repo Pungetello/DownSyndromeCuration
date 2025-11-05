30564dbc14e0d602636041e0ffd16a68ca0239ae
Pungetello <Pungetello@gmail.com>
2025-11-05 13:53:32 -0700

    Working version

--- Main.R
+++ Main.R
@@ -5,11 +5,11 @@ source("DownloadFiles.R")
 #installs packages that are necessary for later steps. These only need to be executed once
 # source("InstallCorePackages.R")
 ##Download Brainarray Examples
-DownloadData(create_unique_vector(platforms_list), "BrainArrayExamples")
+# DownloadData(create_unique_vector(platforms_list), "BrainArrayExamples")
 # source("InstallArrayPackages.R")  #TODO: finish this once website is back up
 
 
 #execute every time:
 DownloadData(names(platforms_list), "Data/Files")
 source("StandardizeMetadata.R")
-# source("RunStatisticalTests.R")
+source("RunStatisticalTests.R")
