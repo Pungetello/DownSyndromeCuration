cdb0a619ae63f86e5a7feb1f019b794ff6f47ab3
Pungetello <Pungetello@gmail.com>
2025-12-03 13:52:02 -0700

    GeneMetadata now makes one file for each dataset instead of one giant one

--- GeneMetadata.R
+++ GeneMetadata.R
@@ -4,18 +4,14 @@ library(biomaRt)
 
 #-----------functions------------------
 
-# returns a vector with the Gene ID's present in all the quality output files
-get_gene_ids_from_quality_output = function(filePath) {
-  file_list = list.files(path = filePath, pattern="\\S+.tsv.gz", full.names= TRUE, ignore.case = TRUE)
-  geneIDs = c()
-  
-  for (file in file_list){
-    file_tibble = read_tsv(file)%>%
-      drop_na()%>%
-      mutate(Sample_ID = str_sub(Sample_ID, 1, -4))%>%
-      dplyr::pull(Sample_ID)
-    geneIDs = c(geneIDs, file_tibble)
-  }
+# returns a vector with the Gene ID's present in the quality output file
+get_gene_ids_from_quality_output = function(file) {
+    
+  file_tibble = read_tsv(file)%>%
+    drop_na()%>%
+    mutate(Sample_ID = str_sub(Sample_ID, 1, -4))%>%
+    dplyr::pull(Sample_ID)
+  geneIDs = c(file_tibble)
   
   unique(geneIDs)%>%
     return()
@@ -23,17 +19,35 @@ get_gene_ids_from_quality_output = function(filePath) {
 
 #-----------script------------------
 
-#get vector of all Gene ID's from NormalizedData
-ensembl_ids = get_gene_ids_from_quality_output(paste0(getwd(), "/Data/NormalizedData"))
+output_file_location = "Data/Metadata/GeneMetadata"
 
-mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+if (!dir.exists(output_file_location)){
+  dir.create(output_file_location, recursive = TRUE)
+}
 
-#use biomaRt to get corresponding data
-gene_metadata = getBM(
-  attributes = c("entrezgene_id", "hgnc_symbol", "ensembl_gene_id", "chromosome_name", "start_position", "end_position"),
-  filters = "ensembl_gene_id",
-  values = ensembl_ids,
-  mart = mart
-)
+filePath = paste0(getwd(), "/Data/NormalizedData")
+file_list = list.files(path = filePath, pattern="\\S+.tsv.gz", full.names= TRUE, ignore.case = TRUE)
 
-write_tsv(gene_metadata, paste0(getwd(), "/Data/Metadata/GeneMetadata.tsv"))
+for(file in file_list){
+
+  #get vector of all Gene ID's from that file
+  ensembl_ids = get_gene_ids_from_quality_output(file)
+  
+  mart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+  
+  #use biomaRt to get corresponding data
+  if(length(ensembl_ids) > 0){
+    gene_metadata = getBM(
+      attributes = c("entrezgene_id", "hgnc_symbol", "ensembl_gene_id", "chromosome_name", "start_position", "end_position"),
+      filters = "ensembl_gene_id",
+      values = ensembl_ids,
+      mart = mart
+    )
+    
+    dataset_name = str_match(file, "\\/([\\w]+[\\d]+)_")[,2]
+    write_tsv(gene_metadata, paste0(getwd(), "/Data/Metadata/GeneMetadata/", dataset_name, "_genes.tsv"))
+  } else {
+    print(paste0("NO GENE IDS IN FILE ", file))
+  }
+
+}
