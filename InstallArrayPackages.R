5dcd35f594ce8cf3238fc5130d49e5b8dcd9a7e2
Pungetello <Pungetello@gmail.com>
2025-12-05 16:33:03 -0700

    Save before project move

--- InstallArrayPackages.R
+++ InstallArrayPackages.R
@@ -6,6 +6,7 @@ library(BiocManager)
 library(SCAN.UPC)
 library(arrayQualityMetrics)
 library(osfr)
+library(archive)
 
 source("PlatformsList.R")
 
@@ -40,131 +41,41 @@ BrainInstallFromOSF = function(project_link, packages){
     
     #tempPackageFilePath = paste0(tempPackagePath, "/", packageFileName)
     tempPackageFilePath = file.path(tempPackagePath, packageFileName)
-    #-----------loading_libraries-----------
-library(GEOquery)
-library(affy)
-library(tidyverse)
-library(BiocManager)
-library(SCAN.UPC)
-library(arrayQualityMetrics)
-library(osfr)
+  }
+}
 
-source("PlatformsList.R")
-
-#-----------functions------------------
-
-BrainInstallFromOSF = function(project_link, packages){
+BrainInstallFromZenodo = function(packages){
   version = "25.0.0"
+  zipFile = paste0(getwd(), "/BrainArray/BrainArrays.zip")
   
-  #creates a temporary directory to hold the packages
-  tmpDir = tempdir()
-  tempPackagePath = file.path(tmpDir)
-  
-  #gets tibble of all the files in the OSF project
-  files = osf_retrieve_node(project_link) %>%
-    osf_ls_files(n_max = Inf)%>%
-    arrange(name) #debug
-  print(files, n=Inf) #debug
-  
-  for (packageName in packages){
-  
-    packageFileName = paste(packageName, "_", version, ".tar.gz",
-                            sep = "")
-    print(packageFileName) #debug
-    #Use the file list to find the file ID of the package
-    packageID = filter(files, name == packageFileName )%>%
-      pull(id)
-    
-    #Download and install package by its ID
-    osf_retrieve_file(packageID) %>%
-      osf_download(tempPackagePath, conflicts = "overwrite")
-    
-    #tempPackageFilePath = paste0(tempPackagePath, "/", packageFileName)
-    tempPackageFilePath = file.path(tempPackagePath, packageFileName)
-    #-----------loading_libraries-----------
-library(GEOquery)
-library(affy)
-library(tidyverse)
-library(BiocManager)
-library(SCAN.UPC)
-library(arrayQualityMetrics)
-library(osfr)
-
-source("PlatformsList.R")
-
-#-----------functions------------------
-
-BrainInstallFromOSF = function(project_link, packages){
-  version = "25.0.0"
-  
-  #creates a temporary directory to hold the packages
-  tmpDir = tempdir()
-  tempPackagePath = file.path(tmpDir)
-  
-  #gets tibble of all the files in the OSF project
-  files = osf_retrieve_node(project_link) %>%
-    osf_ls_files(n_max = Inf)%>%
-    arrange(name) #debug
-  print(files, n=Inf) #debug
-  
-  for (packageName in packages){
-  
-    packageFileName = paste(packageName, "_", version, ".tar.gz",
-                            sep = "")
-    print(packageFileName) #debug
-    #Use the file list to find the file ID of the package
-    packageID = filter(files, name == packageFileName )%>%
-      pull(id)
-    
-    #Download and install package by its ID
-    osf_retrieve_file(packageID) %>%
-      osf_download(tempPackagePath, conflicts = "overwrite")
-    
-    #tempPackageFilePath = paste0(tempPackagePath, "/", packageFileName)
-    tempPackageFilePath = file.path(tempPackagePath, packageFileName)
-
-    install.packages(tempPackageFilePath, repos = NULL, type = "source")
+  if(!file.exists(zipFile)){
+    options(timeout = Inf)
+    download.file("https://zenodo.org/records/17808641/files/BrainArray.zip?download=1", zipFile)
   }
+  
+  packages = unlist(lapply(packages, function(x) paste0(x, "_", version, ".tar.gz")))
+  #pull out package for it from zip
+    
+  print(packages)
+  
+  zipped_names <- archive::archive("BrainArray/BrainArrays.zip")
+  
+  print(zipped_names)
+  
+  archive::archive_extract(zipFile, file = packages, dir = "BrainArray") 
+  
 }
 
   
 #-----------installing_array_packages-----------
 
-packages = unlist(platforms_list) %>%
-  unique()
-packages = packages[!is.na(packages)]
-
-print(packages)
-
-
-BrainInstallFromOSF("b7r3g", packages)
-    install.packages(tempPackageFilePath, repos = NULL, type = "source")
-  }
+if (!dir.exists("BrainArray")){
+  dir.create("BrainArray", recursive = TRUE)
 }
 
-  
-#-----------installing_array_packages-----------
-
 packages = unlist(platforms_list) %>%
   unique()
 packages = packages[!is.na(packages)]
 
-print(packages)
-
-
-BrainInstallFromOSF("b7r3g", packages)
-    install.packages(tempPackageFilePath, repos = NULL, type = "source")
-  }
-}
-
-  
-#-----------installing_array_packages-----------
-
-packages = unlist(platforms_list) %>%
-  unique()
-packages = packages[!is.na(packages)]
-
-print(packages)
-
-
-BrainInstallFromOSF("b7r3g", packages)
+BrainInstallFromZenodo(packages)
+#BrainInstallFromOSF("b7r3g", packages)
--- PlatformsList.R
+++ PlatformsList.R
@@ -9,20 +9,20 @@ platforms_list = list(
   "GSE202938" = NA, 
   "GSE210117" = NA,
   
-  # #Affymetrix datasets
-  # "GSE110064" = "hgu133plus2hsensgprobe",
-  # "GSE48611"= "hgu133plus2hsensgprobe",
-  # "GSE1789" = "hgu133ahsensgprobe",
-  # #"GSE11877" = "hgu133plus2hsensgprobe", #the big one
-  # "GSE16176" = "hgu133plus2hsensgprobe",
-  # "GSE16677" = "hgu133plus2hsensgprobe",
-  # #"GSE17459" = "hgu133plus2hsensgprobe", #c("GPL570", "GPL96"), #does indeed need both, if we use this we'll need to redo stuff
-  # "GSE19680" = "hgu133plus2hsensgprobe",
-  # "GSE19681" = "hgu133plus2hsensgprobe",
-  # "GSE20910" = "hgu133plus2hsensgprobe",
-  # "GSE30517"= "hgu133ahsensgprobe",
-  #"GSE138861" = "clariomshumanhsensgprobe", #"[Clariom_S_Human] Affymetrix Clariom S Assay, Human (Includes Pico Assay) (GPL23159)",
-  #"GSE143885" = "clariomshumanhsensgprobe", #"[Clariom_S_Human] Affymetrix Clariom S Assay, Human (Includes Pico Assay) (GPL23159)",
+  #Affymetrix datasets
+  "GSE110064" = "hgu133plus2hsensgprobe",
+  "GSE48611"= "hgu133plus2hsensgprobe",
+  "GSE1789" = "hgu133ahsensgprobe",
+  #"GSE11877" = "hgu133plus2hsensgprobe", #the big one
+  "GSE16176" = "hgu133plus2hsensgprobe",
+  "GSE16677" = "hgu133plus2hsensgprobe",
+  #"GSE17459" = "hgu133plus2hsensgprobe", #c("GPL570", "GPL96"), #does indeed need both, if we use this we'll need to redo stuff
+  "GSE19680" = "hgu133plus2hsensgprobe",
+  "GSE19681" = "hgu133plus2hsensgprobe",
+  "GSE20910" = "hgu133plus2hsensgprobe",
+  "GSE30517"= "hgu133ahsensgprobe",
+  "GSE138861" = "clariomshumanhsensgprobe", #"[Clariom_S_Human] Affymetrix Clariom S Assay, Human (Includes Pico Assay) (GPL23159)",
+  "GSE143885" = "clariomshumanhsensgprobe", #"[Clariom_S_Human] Affymetrix Clariom S Assay, Human (Includes Pico Assay) (GPL23159)",
   #"GSE149464" = "mogene10stmmensgprobe",
   #"GSE149465" = "mogene10stmmensgprobe"
   "GSE16676" = "mouse4302mmensgprobe"
