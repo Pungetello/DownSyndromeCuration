dbff1ce28ceca0200940e244c643c98db85bbac7
Pungetello <Pungetello@gmail.com>
2026-02-09 13:45:30 -0700

    check if writable, not just if not set

--- InstallCorePackages.R
+++ InstallCorePackages.R
@@ -2,19 +2,22 @@
 #check if can do it one by one
 
 #-----------installing_libraries-----------
+
+#check to see that the library path is writable; if not, change it.
 user_lib <- Sys.getenv("R_LIBS_USER")
-if (user_lib == "") {
+if (user_lib == "" || file.access(user_lib, 2) != 0) {
   user_lib <- "~/R/library"
+  dir.create(user_lib, showWarnings = FALSE, recursive = TRUE)
+  .libPaths(c(user_lib, .libPaths()))
 }
-#dir.create(user_lib, showWarnings = FALSE, recursive = TRUE)
-#.libPaths(c(user_lib, .libPaths()))
+
 print(user_lib)
 
 
 
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(version = "3.22")
+BiocManager::install(version = "3.22", lib = user_lib)
 BiocManager::install(c("tidyverse",
                        "SCAN.UPC",
                        "GEOquery",
@@ -24,7 +27,7 @@ BiocManager::install(c("tidyverse",
                        "Rcurl",
                        "rentrez",
                        "Rsubread"
-                       )
+                       ), lib = user_lib
 )
 
 #-----------Previously_included_libraries-----------
