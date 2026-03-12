2e04761ed6b5076e498d1737f75399370e96e9b9
Pungetello <Pungetello@gmail.com>
2026-03-12 11:47:29 -0600

    first draft for testing

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -15,7 +15,61 @@ library(DESeq2)
 #----------functions-------------
 
 
+#creates a metadata file for deseq2 for the GSE given
+create_metadata = function(gse){
+  GSE_to_SRR = read.tsv("Data/RNA_GSE_to_SRR.tsv")
+  sample_metadata = read.tsv("Data/Metadata/SampleMetadata.tsv")
+  
+  metadata = filter(sample_metadata, Dataset_ID = gse)%>%
+    select(ID, Value)%>%
+    rename(ID, GSE)%>%
+    full_join(filter(GSE_to_SRR, GSE=gse), by = "GSE")%>%
+    select(SRR, Value)%>%
+    print()#debug
+  
+  return(metadata)
+}
 
 
 #----------Differential Expression Analysis-------------
+
+files = list.files(path = "Data/NormalizedData", pattern = "GSE*_gene_counts.csv")
+print(files)#debug
+
+file = "Data/NormalizedData/GSE184771_gene_counts.csv" #debug
+#for (file in files){
+  #get gene_counts for the GRE
+  counts = read.csv(file)
+  
+  #create tibble mapping each sample to 'control_group' or 'affected_group'
+  gse = file.split('/')[3].split('.')[1]
+  print(gse) #debug
+  metadata = create_metadata(gse)
+  
+  #set up input
+  dds = DESeqDataSetFromMatrix(
+    countData = counts,
+    colData = metadata,
+    design = ~ condition
+  )
+  dds$condition = relevel(dds$condition, ref="control_group")
+  
+  #run the analysis
+  dds = DESeq(dds)
+  
+  results = results(dds)
+  
+  #filter results to adjusted p-value < 0.05
+  significant_genes = subset(res, padj < 0.05)
+  
+  #write to file
+  write.tsv(as.data.frame(sig_genes), file=paste0(getwd(), "/Data/NormalizedData/", gse, "_DE.tsv"))
+  
+#}
+
+
+
+
+
+
 
