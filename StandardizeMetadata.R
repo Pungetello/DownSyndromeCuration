eed55ff3f31ac896c598bbfe479beb59b269fa1c
Pungetello <Pungetello@gmail.com>
2026-03-12 12:11:46 -0600

    change library path for supercomputer

--- StandardizeMetadata.R
+++ StandardizeMetadata.R
@@ -1,3 +1,12 @@
+#change library path for supercomputer
+user_lib <- Sys.getenv("R_LIBS_USER")
+if (user_lib == "" || file.access(user_lib, 2) != 0) {
+  user_lib <- "~/R/library"
+  dir.create(user_lib, showWarnings = FALSE, recursive = TRUE)
+  .libPaths(c(user_lib, .libPaths()))
+}
+
+
 #-----------loading_libraries-----------
 library(GEOquery)
 library(tidyverse)
