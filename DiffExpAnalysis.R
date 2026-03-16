e445540a9a7f395e5ccf850fe29c8dc5846c3ff6
Pungetello <Pungetello@gmail.com>
2026-03-16 13:46:12 -0600

    change to matrix

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -45,6 +45,7 @@ file = "Data/NormalizedData/GSE202938_gene_counts.csv" #debug
   counts = as.data.frame(counts)
   rownames(counts) = counts$gene_id
   counts$gene_id = NULL
+  counts = as.matrix(counts)
   print(counts, n=10) #debug
   
   #create tibble mapping each sample to 'control_group' or 'affected_group'
