c28fc88d35bfe3efb4d56c0aadf79aaf75690eed
Pungetello <Pungetello@gmail.com>
2026-03-17 10:45:29 -0600

    no color-coding for significance for now

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -39,11 +39,11 @@ create_metadata = function(gse){
 
 #create a volcano plot of the data
 volcano_plot = function(data, output_prefix){
+  
   top <- data[order(data$padj), ][1:10, ]
   
-  ggplot(data, aes(x = log2FoldChange, y = -log10(padj), color=significant)) +
+  ggplot(data, aes(x = log2FoldChange, y = -log10(padj))) +
     geom_point(alpha = 0.5) +
-    scale_color_manual(values = c("grey", "blue")) +
     geom_text_repel(data = top, aes(label = gene)) +
     theme_minimal()
   
