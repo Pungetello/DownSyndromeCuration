4c4272736a2b313707fa112ab03eddc8a2b6e126
Pungetello <Pungetello@gmail.com>
2026-04-17 16:13:39 -0600

    filter for debug

--- ProcessRNAData.R
+++ ProcessRNAData.R
@@ -202,6 +202,10 @@ GSE_to_SRR = read_tsv(paste0(getwd(), "/Data/RNA_GSE_to_SRR.tsv"))
 
 for (srr in srrs){
   print(srr)
+  if(!startsWith(srr, "SRR5")){
+    next()
+  }
+  
   geo_id = GSE_to_SRR$GSE[GSE_to_SRR$SRR == srr]
   print(geo_id)
   
