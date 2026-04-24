a1b6f91cdedd35d5d1431040045750c927204331
Pungetello <Pungetello@gmail.com>
2026-04-24 10:15:29 -0600

    combining files only

--- ProcessRNAData.R
+++ ProcessRNAData.R
@@ -194,58 +194,58 @@ combine_files = function(gse, srrs, suffix){
 
 #--------------process_RNA_data-------------
 
-#get list of all srr files prefetched by previous script
-srrs = list.files("Data/RawRNA")
-print(srrs)
-
-GSE_to_SRR = read_tsv(paste0(getwd(), "/Data/RNA_GSE_to_SRR.tsv"))
-
-for (srr in srrs){
-  print(srr)
-  # if(!startsWith(srr, "SRR5")){
-  #   next()
-  # }
-  
-  geo_id = GSE_to_SRR$GSE[GSE_to_SRR$SRR == srr]
-  print(geo_id)
-  
-  if(length(geo_id)==0){
-    print("NO GEOID FOUND")
-    next()
-  }
-  
-  if(Datasets$Organism[Datasets$Name == geo_id] == "human"){
-    
-    next()
-
-    #human stuff
-    ref = paste0(getwd(), "/RefGenomes/GRCh38_ref.fna.gz")
-    annotation = paste0(getwd(), "/RefGenomes/49_ann.gtf.gz")
-    index = "GRCh38_index"
-  }
-  
-  else if(Datasets$Organism[Datasets$Name == geo_id] == "mouse"){
-    
-    # #normal mouse stuff
-    # ref = paste0(getwd(), "/RefGenomes/GRCm39_ref.fna.gz")
-    # annotation = paste0(getwd(), "/RefGenomes/M38_ann.gtf.gz")
-    # index = "GRCm39_index"
-    
-    #mouse with MAC
-    ref = paste0(getwd(), "/RefGenomes/mouse_plus_mac.fa")
-    annotation = paste0(getwd(), "/RefGenomes/mouse_plus_mac.gtf.gz")
-    index = "mouse_plus_mac_index"
-  }
-
-  #finish installation by converting to fastq format
-  install_raw(srr)
-
-  #build gene index if not already present
-  build_index(index, ref)
-
-  #run alignment and save output files
-  process_data(srr, index, annotation)
-}
+# #get list of all srr files prefetched by previous script
+# srrs = list.files("Data/RawRNA")
+# print(srrs)
+# 
+# GSE_to_SRR = read_tsv(paste0(getwd(), "/Data/RNA_GSE_to_SRR.tsv"))
+# 
+# for (srr in srrs){
+#   print(srr)
+#   # if(!startsWith(srr, "SRR5")){
+#   #   next()
+#   # }
+#   
+#   geo_id = GSE_to_SRR$GSE[GSE_to_SRR$SRR == srr]
+#   print(geo_id)
+#   
+#   if(length(geo_id)==0){
+#     print("NO GEOID FOUND")
+#     next()
+#   }
+#   
+#   if(Datasets$Organism[Datasets$Name == geo_id] == "human"){
+#     
+#     next()
+# 
+#     #human stuff
+#     ref = paste0(getwd(), "/RefGenomes/GRCh38_ref.fna.gz")
+#     annotation = paste0(getwd(), "/RefGenomes/49_ann.gtf.gz")
+#     index = "GRCh38_index"
+#   }
+#   
+#   else if(Datasets$Organism[Datasets$Name == geo_id] == "mouse"){
+#     
+#     # #normal mouse stuff
+#     # ref = paste0(getwd(), "/RefGenomes/GRCm39_ref.fna.gz")
+#     # annotation = paste0(getwd(), "/RefGenomes/M38_ann.gtf.gz")
+#     # index = "GRCm39_index"
+#     
+#     #mouse with MAC
+#     ref = paste0(getwd(), "/RefGenomes/mouse_plus_mac.fa")
+#     annotation = paste0(getwd(), "/RefGenomes/mouse_plus_mac.gtf.gz")
+#     index = "mouse_plus_mac_index"
+#   }
+# 
+#   #finish installation by converting to fastq format
+#   install_raw(srr)
+# 
+#   #build gene index if not already present
+#   build_index(index, ref)
+# 
+#   #run alignment and save output files
+#   process_data(srr, index, annotation)
+# }
 
 
 combine_results_per_GSE()
