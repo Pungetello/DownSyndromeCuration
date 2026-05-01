825762e36e6a5a3fcb1c9f28f85d816fdba8a5df
Pungetello <Pungetello@gmail.com>
2026-04-30 19:35:51 -0600

    factoring aproach

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -58,7 +58,8 @@ volcano_plot = function(data, output_prefix){
   
   print(head(data))
   
-  ggplot(data, aes(x = log2FoldChange, y = -log10(padj), color = chromosome_name=="21")) +
+  ggplot(data, aes(x = log2FoldChange, y = -log10(padj), color = factor(chromosome_name == "21",
+                                                                        labels = c("Other", "Chr21")))) +
     theme(plot.background = element_rect(fill = "white"))+
     geom_point(alpha = 0.5) +
     #scale_color_manual(values = c("grey", "blue")) +
