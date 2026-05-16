f5d2776ce1a269073ca892607b37620fce70c4bd
Pungetello <Pungetello@gmail.com>
2026-05-15 23:14:04 -0600

    explicitly creating factor to fix NA in key

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -66,13 +66,14 @@ volcano_plot = function(graph_data, output_prefix, file){
   
   if(grepl("MAC", file)){
     graph_data$chr21_flag = ifelse(graph_data$chromosome_name == "21", "Chr-21", 
-                                   ifelse(graph_data$chromosome_name == "16", "Chr-16", 
-                                          "Other"))
-    # print("GENES ON CHR16:")
-    # print(head(filter(graph_data, chromosome_name == "16")))
+                            ifelse(graph_data$chromosome_name == "16", "Chr-16", 
+                            "Other"))
+    graph_data$chr21_flag = factor(graph_data$chr21_flag, levels = c("Chr-21", "Chr-16", "Other"))
+
   }else{
-    graph_data$chr21_flag = ifelse(graph_data$chromosome_name == "21",
-                                   "Chr-21", "Other")
+    graph_data$chr21_flag = ifelse(graph_data$chromosome_name == "21", "Chr-21", 
+                            "Other")
+    graph_data$chr21_flag = factor(graph_data$chr21_flag, levels = c("Chr-21", "Other"))
   }
   
   graph_data = graph_data[
