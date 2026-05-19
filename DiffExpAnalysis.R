3d69670fbb5759d40ebe38df08822e42fb959fdc
Pungetello <Pungetello@gmail.com>
2026-05-18 23:12:38 -0600

    debug

--- DiffExpAnalysis.R
+++ DiffExpAnalysis.R
@@ -68,14 +68,16 @@ volcano_plot = function(graph_data, output_prefix, file){
     graph_data$chr21_flag = ifelse(graph_data$chromosome_name == "21", "Chr-21", 
                             ifelse(graph_data$chromosome_name == "16", "Chr-16", 
                             "Other"))
-    print(table(graph_data$chromosome_name, useNA = "always"))
+    print("NAs:")
+    print(filter(graph_data, is.na(chromosome_name)), width=Inf)
     print(unique(pull(graph_data, chr21_flag)))
     graph_data$chr21_flag = factor(graph_data$chr21_flag, levels = c("Chr-21", "Chr-16", "Other"))
 
   }else{
     graph_data$chr21_flag = ifelse(graph_data$chromosome_name == "21", "Chr-21", 
                             "Other")
-    print(table(graph_data$chromosome_name, useNA = "always"))
+    print("NAs:")
+    print(filter(graph_data, is.na(chromosome_name)), width=Inf)
     print(unique(pull(graph_data, chr21_flag)))
     graph_data$chr21_flag = factor(graph_data$chr21_flag, levels = c("Chr-21", "Other"))
   }
