f753111df413b13c902204c9340a35234978f320
Pungetello <Pungetello@gmail.com>
2026-06-08 10:29:54 -0600

    reordered version for continuing detective work

--- ProcessRNAData.R
+++ ProcessRNAData.R
@@ -101,7 +101,7 @@ process_data = function(srr, index, annotation, mac){
   counts_df = rename(counts_df, !!srr := paste0(srr,"_AlignResults.BAM")) %>%
     select("gene_id", srr)
   
-  write_tsv(counts_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_gene_counts.tsv"))
+  write_tsv(counts_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_gene_counts_reordered.tsv"))
   
   calculate_data_files(feature_counts, mac)
   
@@ -125,7 +125,7 @@ calculate_data_files = function(feature_counts, mac){
   rpkm_df = rownames_to_column(as.data.frame(rpkm), "gene_id")
   colnames(rpkm_df)[2] <- srr
   
-  write_tsv(rpkm_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_RPKM.tsv"))
+  write_tsv(rpkm_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_RPKM_reordered.tsv"))
   
   #calculate tpm
   length_kb <- gene_length / 1000
@@ -135,7 +135,7 @@ calculate_data_files = function(feature_counts, mac){
   tpm_df = rownames_to_column(as.data.frame(tpm), "gene_id")
   colnames(tpm_df)[2] <- srr
   
-  write_tsv(tpm_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_TPM.tsv"))
+  write_tsv(tpm_df, file=paste0(getwd(), "/Data/NormalizedData/", srr, mac, "_TPM_reordered.tsv"))
 }
 
 
@@ -159,9 +159,9 @@ combine_results_per_GSE = function(){
       pull(SRR)
     
     
-    combine_files(gse, srrs, paste0(mac, "_gene_counts.tsv"))
-    combine_files(gse, srrs, paste0(mac, "_TPM.tsv"))
-    combine_files(gse, srrs, paste0(mac, "_RPKM.tsv"))
+    combine_files(gse, srrs, paste0(mac, "_gene_counts_reordered.tsv"))
+    combine_files(gse, srrs, paste0(mac, "_TPM_reordered.tsv"))
+    combine_files(gse, srrs, paste0(mac, "_RPKM_reordered.tsv"))
     
   }
 }
@@ -212,6 +212,9 @@ for (srr in srrs){
   if(length(geo_id)==0){
     print("NO GEOID FOUND")
     next()
+  }else if(sum(geo_id == c("GSE109293","GSE109294","GSE202938","GSE210117")) == 0){
+    print("NOT SELECTED GSE, SKIPPING")
+    next()
   }
 
   MAC = ""
@@ -233,13 +236,13 @@ for (srr in srrs){
 
     MAC = "_MAC"
     #mouse with MAC
-    ref = paste0(getwd(), "/RefGenomes/mouse_plus_mac.fa")
+    ref = paste0(getwd(), "/RefGenomes/mouse_plus_mac_reordered.fa")
     annotation = paste0(getwd(), "/RefGenomes/mouse_plus_mac.gtf.gz")
     index = "mouse_plus_mac_index"
   }
 
   #check if alignment has already been done
-  if (file.exists(paste0(getwd(), "/Data/NormalizedData/", srr, MAC, "_RPKM.tsv"))){
+  if (file.exists(paste0(getwd(), "/Data/NormalizedData/", srr, MAC, "_RPKM_reordered.tsv"))){
     print("OUTPUT ALREADY EXISTS, SKIPPING")
     next()
   }
