86da20558b65101f832e8bffccbaa2923e8a8d11
Pungetello <pungetello@gmail.com>
2026-08-04 12:07:27 -0600

    checking detective code one more time

--- ProcessRNAData.R
+++ ProcessRNAData.R
@@ -279,20 +279,23 @@ for (srr in srrs){
 
 combine_results_per_dataset()
 
-# #detective work code, delete later
-# 
-# for(geo_id in c("GSE109293","GSE109294","GSE202938","GSE210117")){
-#   TcMAC_genes = read_tsv(paste0(getwd(), "/Data/NormalizedData/", geo_id, "_MAC_fixed_gene_counts.tsv"))%>%
-#     filter(grepl("ENSG", gene_id))
-#   
-#   sum(select(TcMAC_genes, !gene_id))%>%
-#     print()
-#   
-#   read_tsv(paste0(getwd(), "/Data/NormalizedData/", geo_id, "_human_gene_counts.tsv"))%>%
-#     inner_join(select(TcMAC_genes, gene_id))%>%
-#     print()%>%
-#     select(!gene_id)%>%
-#     sum()%>%
-#     print()
-# }
+#detective work code, delete later
+
+for(geo_id in c("GSE109293","GSE109294","GSE202938","GSE210117")){
+  print(geo_id)
+  TcMAC_genes = read_tsv(paste0(getwd(), "/Data/NormalizedData/", geo_id, "_MAC_fixed_gene_counts.tsv"))%>%
+    filter(grepl("ENSG", gene_id))
+
+  print("NUMBER OF HUMAN GENE MATCHES FOR MAC ALIGNMENT:")
+  sum(select(TcMAC_genes, !gene_id))%>%
+    print()
+
+  print("NUMBER OF HUMAN GENE MATCHES FOR HUMAN ALIGNMENT:")
+  read_tsv(paste0(getwd(), "/Data/NormalizedData/", geo_id, "_human_gene_counts.tsv"))%>%
+    inner_join(select(TcMAC_genes, gene_id))%>%
+    print()%>%
+    select(!gene_id)%>%
+    sum()%>%
+    print()
+}
 
